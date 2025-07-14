31ad877795cc71ce07c467654e467bc7e251adf4
Depayss <Christophermaomao@gmail.com>
2025-07-14 11:14:37 -0400

    play piano at the same time as the beat!!!!!!!

--- frontend/src/components/Keyboard.jsx
+++ frontend/src/components/Keyboard.jsx
@@ -1,91 +1,55 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Keyboard() {
-  function a() {
-    const sound = new Audio(`/keyboard-sounds/a1.wav`);
-    sound.play();
-  }
-  function as() {
-    const sound = new Audio(`/keyboard-sounds/a1s.wav`);
-    sound.play();
-  }
-  function b() {
-    const sound = new Audio(`/keyboard-sounds/b1.wav`);
-    sound.play();
-  }
-  function c() {
-    const sound = new Audio(`/keyboard-sounds/c1.wav`);
-    sound.play();
-  }
-  function cs() {
-    const sound = new Audio(`/keyboard-sounds/c1s.wav`);
-    sound.play();
-  }
-  function d() {
-    const sound = new Audio(`/keyboard-sounds/d1.wav`);
-    sound.play();
-  }
-  function ds() {
-    const sound = new Audio(`/keyboard-sounds/d1s.wav`);
-    sound.play();
-  }
-  function e() {
-    const sound = new Audio(`/keyboard-sounds/e1.wav`);
-    sound.play();
-  }
-  function f() {
-    const sound = new Audio(`/keyboard-sounds/f1.wav`);
-    sound.play();
-  }
-  function fs() {
-    const sound = new Audio(`/keyboard-sounds/f1s.wav`);
-    sound.play();
-  }
-  function g() {
-    const sound = new Audio(`/keyboard-sounds/g1.wav`);
-    sound.play();
-  }
-  function gs() {
-    const sound = new Audio(`/keyboard-sounds/g1s.wav`);
-    sound.play();
-  }
+  const keySounds = useRef({}) // intentionally left empty
 
-  useEffect(() => {
-    document.addEventListener("keydown", handleKey, true);
-    function handleKey(ev) {
-      if (ev.key === "a") c();
-      else if (ev.key === "w") cs();
-      else if (ev.key === "s") d();
-      else if (ev.key === "e") ds();
-      else if (ev.key === "d") e();
-      else if (ev.key === "f") f();
-      else if (ev.key === "t") fs();
-      else if (ev.key === "g") g();
-      else if (ev.key === "y") gs();
-      else if (ev.key === "h") a();
-      else if (ev.key === "j") as();
-      else if (ev.key === "u") b();
-    }
-  });
+  // The keySounds object will be populated with new Audio objects
+  // when the app is opened, and each keyboard-press will play that
+  // existing Audio object, avoiding creating a new one upon every
+  // re-render. Apparently, a browser limits the amount of Audio
+  // objects you can have, depending on how many files it can load
+  // without timing out due to too many requests. (According to SO)
+
+  // Populating the keySounds object.
+  useEffect(()=>{
+    keySounds.current["a"] = new Audio("/keyboard-sounds/a1.wav")
+    keySounds.current["as"] = new Audio("/keyboard-sounds/a1s.wav")
+    keySounds.current["b"] = new Audio("/keyboard-sounds/b1.wav")
+    keySounds.current["c"] = new Audio("/keyboard-sounds/c1.wav")
+    keySounds.current["cs"] = new Audio("/keyboard-sounds/c1s.wav")
+    keySounds.current["d"] = new Audio("/keyboard-sounds/d1.wav")
+    keySounds.current["ds"] = new Audio("/keyboard-sounds/d1s.wav")
+    keySounds.current["e"] = new Audio("/keyboard-sounds/e1.wav")
+    keySounds.current["f"] = new Audio("/keyboard-sounds/f1.wav")
+    keySounds.current["fs"] = new Audio("/keyboard-sounds/f1s.wav")
+    keySounds.current["g"] = new Audio("/keyboard-sounds/g1.wav")
+    keySounds.current["gs"] = new Audio("/keyboard-sounds/g1s.wav")
+  }, [])
+
+  function playKey(key){
+    const sound = keySounds.current[key]
+    sound.play()
+  }
+  
   return (
     <div className="keyboard-container">
       <div className="white-key-container">
-        <button className="white-key" onClick={c}></button>
-        <button className="white-key" onClick={d}></button>
-        <button className="white-key" onClick={e}></button>
-        <button className="white-key" onClick={f}></button>
-        <button className="white-key" onClick={g}></button>
-        <button className="white-key" onClick={a}></button>
-        <button className="white-key" onClick={b}></button>
+        <button className="white-key" onClick={()=>playKey("c")}></button>
+        <button className="white-key" onClick={()=>playKey("d")}></button>
+        <button className="white-key" onClick={()=>playKey("e")}></button>
+        <button className="white-key" onClick={()=>playKey("f")}></button>
+        <button className="white-key" onClick={()=>playKey("g")}></button>
+        <button className="white-key" onClick={()=>playKey("a")}></button>
+        <button className="white-key" onClick={()=>playKey("b")}></button>
       </div>
       <div className="black-key-container1">
-        <button className="black-key" onClick={cs}></button>
-        <button className="black-key" onClick={ds}></button>
+        <button className="black-key" onClick={()=>playKey("cs")}></button>
+        <button className="black-key" onClick={()=>playKey("ds")}></button>
       </div>
       <div className="black-key-container2">
-        <button className="black-key" onClick={fs}></button>
-        <button className="black-key" onClick={gs}></button>
-        <button className="black-key" onClick={as}></button>
+        <button className="black-key" onClick={()=>playKey("fs")}></button>
+        <button className="black-key" onClick={()=>playKey("gs")}></button>
+        <button className="black-key" onClick={()=>playKey("as")}></button>
       </div>
     </div>
   );
